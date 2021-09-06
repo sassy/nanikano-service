961a50062c8b3286d21d379e42a994a11ade06b5
Satoshi Watanabe <sassy.watanabe@gmail.com>
2021-09-06 23:23:32 +0900

    ちょっとstyled-componentを試す

---
+++ components/Footer.tsx
@@ -0,0 +1,16 @@
+import styled from "styled-components"
+
+const StyuledFooter = styled.footer`
+    width: 100%;
+    height: 100px;
+    border-top: 1px solid #eaeaea;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`
+
+export const Footer = () => (
+    <footer>
+          created　by Sassy
+    </footer>
+)
